ce6ac68257e7325921397061a791c2c8ef144a0a
JavierGarciaEstrada <baevaj@naver.com>
2023-03-09 14:53:10 -0600

    Practica Cuenta Bancaria

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,11 +1,9 @@
-
-
 import java.util.Scanner;
 public class CuentaBancaria {
     public static int menu (){
         Scanner sc = new Scanner (System.in);
         int opc1 = 0;
-        System.out.println("[1] Ver el Saldo");
+        System.out.println("[1] Ver informacion de la cuenta");
         System.out.println("[2] Realizar un deposito");
         System.out.println("[3] Realizar un retiro");
         System.out.println("[0] Salir");
@@ -18,40 +16,24 @@ public class CuentaBancaria {
         }
         return opc1;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opc1 = 0;
-
-        DatosDeCuenta c1 = new DatosDeCuenta("Javier", "Garcia", 71243, 12332);
-
-
+        DatosDeCuenta c1 = new DatosDeCuenta("Javier", "Garcia", 0, 1234567890);
+        
         do {
             opc1 = menu();
-            //si se pone otra cosa solo se ejecuta el menu en forma de ciclo
-
             switch (opc1) {
-
                 case 1:
-
                     c1.toString();
-                    System.out.println(" ");
                     break;
                 case 2:
-
-                    System.out.println("Ingrese la cantidad a depositar");
-                    c1.deposit(sc.nextDouble());
-
+                    c1.deposit();
                     break;
-
                 case 3:
-
-                    System.out.println("Ingrese la cantidad a retirar");
-                    c1.withdraw(sc.nextDouble());
-
+                    c1.withdraw();
                     break;
-
-                case 0:
+                default:
                     System.out.println("Gracias por usarnos");
                     break;
             }
--- src/DatosDeCuenta.java
+++ src/DatosDeCuenta.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
+
 public class DatosDeCuenta {
+    public static Scanner sc = new Scanner (System.in);
     private String name;
     private String lastname;
     private double balance;
     private int accountNumber;
-
+//metodo constructor
     public DatosDeCuenta(String name, String lastName, double balance, int accountNumber) {
         this.name = name;
         this.lastname = lastName;
         this.balance = balance;
         this.accountNumber = accountNumber;
     }
+    //getters and setters
     public String getName() {
             return name;
     }
@@ -42,14 +46,19 @@ public class DatosDeCuenta {
             this.accountNumber = accountNumber;
     }
 
-    public void deposit(double amount) {
+    public void deposit() {
+            double amount = 0;
+            System.out.println("Ingrese la cantidad a depositar");
+            amount = sc.nextDouble();
             balance += amount;
             System.out.println("$" + amount + " depositado con éxito.");
             System.out.println("El saldo actual es: $"+ getBalance());
             System.out.println(" ");
         }//incrementa el saldo inicial dependiendo del monto indicado
 
-    public void withdraw(double amount) {
+    public void withdraw() {
+        System.out.println("Ingrese la cantidad a retirar");
+        double amount = sc.nextDouble();
         if (balance >= amount) { //ve si hay dinero suficiente en la cuenta para realizar el retiro
             balance -= amount;
             System.out.println("$" + amount + " retirado con éxito.");
@@ -62,13 +71,13 @@ public class DatosDeCuenta {
             //se proyecta cuendo no hay dinero suficiente en la cuenta para realizar el retiro
         }
     }
-
-
     public String toString() {
         System.out.println("Titular de la cuenta: " + this.name + " " + this.lastname);
         System.out.println("Numero de la cuenta: " + this.accountNumber);
         System.out.println("Saldo de la cuenta: " + this.balance);
+        System.out.println(" ");
         return "";
+        //muestra la informacion de la cuenta
     }
 
 }
